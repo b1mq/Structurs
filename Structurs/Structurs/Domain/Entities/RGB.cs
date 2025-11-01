0eee7c55cac201740ac1a3b4810a5f9f1116138e
Egor <arnoldtagirov1@gmail.com>
2025-11-01 22:41:19 +0100

    -m

---
+++ Structurs/Structurs/Domain/Entities/RGB.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Structurs.Domain.Entities
+{
+    struct RGB
+    {
+        public int Red {  get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+        public RGB(int red, int green, int blue)
+        {
+            if ( red >= 0 && green >= 0 && blue >= 0 && red <= 255 && green <= 255 && blue <= 255)
+            {
+                Red = red;
+                Green = green;
+                Blue = blue;
+            }else
+            {
+                throw new ArgumentException("Some of the color is negative or more than 255");
+            }
+
+        }
+
+        public  string RGBtoHEX()
+        {
+            return "#" + Red.ToString("X2") + Green.ToString("X2") + Blue.ToString("X2");
+        }
+    }
+}
---
+++ Structurs/Structurs/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using Structurs.Domain.Entities;
+
+var rgb = new RGB(255, 87, 51);
+
+Console.WriteLine(rgb.RGBtoHEX());
